e82089bbfed8d544795d08e7ced4aa916f0216b1
Conrad Harrison <a.conradharrison@gmail.com>
2017-12-28 16:39:11 -0600

    Update app.js
    corrected pre-pended start time to 120 seconds

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,7 +57,7 @@ var game = {
 
   start: function() {
     timer = setInterval(game.countdown, 1000);
-    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">120</span> Seconds</h2>');
     $('#start').remove();
 
     for (var i = 0; i < questions.length; i++) {
